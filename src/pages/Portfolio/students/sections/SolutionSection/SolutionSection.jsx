794385ce0d27cc4ed76c1aaa10355115a8e92b1b
prskillverse-afk <prskillverse@gmail.com>
2026-02-07 06:57:23 +0530

    Fix SolutionSection imports and update components

--- src/pages/Portfolio/students/sections/SolutionSection/SolutionSection.jsx
+++ src/pages/Portfolio/students/sections/SolutionSection/SolutionSection.jsx
@@ -1,9 +1,8 @@
 import "./SolutionSection.css";
 import SolutionHero from "./SolutionHero";
-import SolutionCards from "./SolutionCards";
+
 import SolutionCompare from "./SolutionCompare";
-import SloutionCTA from "./SolutionCTA";
-import SolutionCTA from "./SolutionCTA";
+
 
 export default function SolutionSection() {
   return (
